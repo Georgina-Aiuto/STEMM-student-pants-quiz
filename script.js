e284287c1341ffed2512cce694a3fa470168887d
Georgina Aiuto <georgina.aiuto@august.com.au>
2021-05-31 15:21:06 +1000

    12 questions created

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 //starting quiz button
 
-var button = document.getElementById("startQuiz");
+var buttonQuiz = document.getElementById("startQuiz");
 var hover = document.getElementsByClassName("button");
+var buttonHome = document.getElementById("homePage");
+var buttonSubmit = document.getElementById("submit");
 
 //button command
-button.onclick = function() {
+buttonQuiz.onclick = function() {
     location.href="quiz.html";  
 };
+
+buttonHome.onclick = function() {
+    location.href="index.html";  
+};
+
+//calculator 
+
+
+
+buttonHome.onclick = function() {
+    if (jeans > chinos, jeans > tracksuits){
+        location.href="jeans-results.html";
+    }
+    else if (chinos > jeans, chinos > tracksuits){
+        location.href="chinos-results.html";
+    }
+    else if (tracksuits > jeans, tracksuits > chinos){
+        location.href="tracksuits-results.html";
+    }
+    else 
+        location.href="allrounder-results.html";
+};
+
 
 //hover state
 hover.focus();
